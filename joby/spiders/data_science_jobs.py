96b9c25268e98e9464d8b068aa12de113ad1c66f
cyberbikepunk <loic@cyberpunk.bike>
2016-04-18 14:34:01 +0200

    Add more rules for test purposes.

--- joby/spiders/data_science_jobs.py
+++ joby/spiders/data_science_jobs.py
@@ -1,13 +1,21 @@
 # -*- coding: utf-8 -*-
-import scrapy
+
+from logging import getLogger
+from scrapy.spiders import Rule, CrawlSpider
+from scrapy.linkextractors import LinkExtractor
 
 
-class DataScienceJobsSpider(scrapy.Spider):
-    name = "data-science-jobs"
-    allowed_domains = ["www.data-science-jobs.com"]
-    start_urls = (
-        'http://www.data-science-jobs.com/',
-    )
+class DataScienceJobsSpider(CrawlSpider):
+    log = getLogger(__name__)
+    name = 'data-science-jobs'
+    allowed_domains = ['www.data-science-jobs.com', 'fonts.googleapis.com', 'jobs.lever.com']
+    start_urls = ['http://www.data-science-jobs.com/']
+    test = Rule(LinkExtractor(allow='family'), callback='parse')
+    test2 = Rule(LinkExtractor(allow='comtravo'), callback='parse')
+    job_links = Rule(LinkExtractor(allow='detail\/'), callback='parse')
+    pagination_links = Rule(LinkExtractor(allow='\?page=\d+'), callback='parse')
+    rules = [job_links, pagination_links, test, test2]
+    response = None
 
     def parse(self, response):
-        pass
+        self.log.info('Parsing %s', response.url)
