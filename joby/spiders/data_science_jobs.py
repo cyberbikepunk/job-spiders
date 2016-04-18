ce873b24318fd6493f570f370db1d2c2d244bdcc
cyberbikepunk <loic@cyberpunk.bike>
2016-04-18 18:38:05 +0200

    Rename the parser function to parse_jobs.

--- joby/spiders/data_science_jobs.py
+++ joby/spiders/data_science_jobs.py
@@ -8,14 +8,13 @@ from scrapy.linkextractors import LinkExtractor
 class DataScienceJobsSpider(CrawlSpider):
     log = getLogger(__name__)
     name = 'data-science-jobs'
-    allowed_domains = ['www.data-science-jobs.com', 'fonts.googleapis.com', 'jobs.lever.com']
-    start_urls = ['http://www.data-science-jobs.com/']
-    test = Rule(LinkExtractor(allow='family'), callback='parse')
-    test2 = Rule(LinkExtractor(allow='comtravo'), callback='parse')
-    job_links = Rule(LinkExtractor(allow='detail\/'), callback='parse')
-    pagination_links = Rule(LinkExtractor(allow='\?page=\d+'), callback='parse')
-    rules = [job_links, pagination_links, test, test2]
-    response = None
+    allowed_domains = ['www.data-science-jobs.com']
+    start_urls = ['http://www.data-science-jobs.com']
+    job_links = Rule(LinkExtractor(allow='detail\/'), callback='parse_job')
+    pagination_links = Rule(LinkExtractor(allow='page=\d+'))
+    rules = [job_links, pagination_links]
 
-    def parse(self, response):
+    def parse_job(self, response):
         self.log.info('Parsing %s', response.url)
+
+
